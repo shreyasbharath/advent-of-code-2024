18d79c3d66d861f28a7738f76247ba75086a7d55
Shreyas Balakrishna <shreyasbharath@users.noreply.github.com>
2024-12-06 13:57:54 +1100

    progress

--- day_2/day_2_part_2.ts
+++ day_2/day_2_part_2.ts
@@ -1,11 +1,16 @@
 import { parseNuclearReports } from '../day_2/nuclear_report_parser';
 import { Dampen, ReportClassification, classifyReportSafety } from './nuclear_report_safety_classifier';
 
-let { reports } = parseNuclearReports('./day_2/data.csv');
+let { reports } = parseNuclearReports('./day_2/edge_cases.csv');
 
 const classified_reports = classifyReportSafety(reports, Dampen.True);
 
-console.log(classified_reports);
+// console.log(classified_reports);
+
+let dampenedReports = classified_reports.filter(function (classified_report) {
+    return classified_report.dampened;
+});
+console.log(dampenedReports);
 
 let numSafeReports = classified_reports.reduce(function (accumulator, classified_report) {
     return accumulator + (classified_report.classification == ReportClassification.Safe ? 1 : 0);
--- day_2/nuclear_report_safety_classifier.test.ts
+++ day_2/nuclear_report_safety_classifier.test.ts
@@ -11,59 +11,59 @@ test("returns no classified reports if given an empty reports list", () => {
 test("returns unsafe if given a report that is increasing and decreasing", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [7, 6, 4, 2, 1, 5] }]);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Unsafe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Unsafe]);
 });
 
 test("returns unsafe if given a report that is None increasing nor decreasing", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [1, 1, 1, 1, 1] }]);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Unsafe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Unsafe]);
 });
 
 test("returns safe if given a report that is increasing by a margin of <= 3", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [1, 2, 3, 4, 7] }]);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Safe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Safe]);
 });
 
 test("returns unsafe if given a report that is increasing by a margin of > 3", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [1, 2, 3, 4, 8] }]);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Unsafe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Unsafe]);
 });
 
 test("returns safe if given a report that is decreasing by a margin of <= 3", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [8, 5, 3, 2, 1] }]);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Safe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Safe]);
 });
 
 test("returns unsafe if given a report that is decreasing by a margin of > 3", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [8, 3, 2, 1, 0] }]);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Unsafe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Unsafe]);
 });
 
 test("returns safe if dropping one bad level leads to increasing by a margin of <= 3", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [1, 3, 2, 4, 5] }], Dampen.True);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Safe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Safe]);
 });
 
 test("returns safe if dropping one bad level at the start leads to increasing by a margin of <= 3", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [45, 40, 42, 43, 44, 47, 48] }], Dampen.True);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Safe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Safe]);
 });
 
 test("returns safe if dropping one bad level at the end leads to increasing by a margin of <= 3", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [37, 40, 42, 43, 44, 47, 51] }], Dampen.True);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Safe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Safe]);
 });
 
 test("returns safe if dropping one bad level leads to decreasing by a margin of <= 3", () => {
     const classified_reports = classifyReportSafety([<Report>{ levels: [8, 6, 4, 4, 1] }], Dampen.True);
 
-    expect(classified_reports).toStrictEqual([ReportClassification.Safe]);
+    expect(classified_reports.map(report => report.classification)).toStrictEqual([ReportClassification.Safe]);
 });
