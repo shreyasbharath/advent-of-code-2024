ec177e1895b8cb689c1c607586efdee52edab139
Shreyas Balakrishna <shreyasbharath@users.noreply.github.com>
2024-12-06 13:41:09 +1100

    add logging

--- day_2/day_2_part_2.ts
+++ day_2/day_2_part_2.ts
@@ -5,8 +5,9 @@ let { reports } = parseNuclearReports('./day_2/data.csv');
 
 const classified_reports = classifyReportSafety(reports, Dampen.True);
 
-let numSafeReports = classified_reports.reduce(function (accumulator, classified_report) {
-    return accumulator + (classified_report == ReportClassification.Safe ? 1 : 0);
-}, 0);
+console.log(classified_reports);
 
+let numSafeReports = classified_reports.reduce(function (accumulator, classified_report) {
+    return accumulator + (classified_report.classification == ReportClassification.Safe ? 1 : 0);
+}, 0);
 console.log(`${numSafeReports} safe out of ${classified_reports.length} reports`);
--- day_2/nuclear_report_safety_classifier.ts
+++ day_2/nuclear_report_safety_classifier.ts
@@ -6,6 +6,12 @@ export enum ReportClassification {
     Safe = "safe",
 }
 
+export type ClassifiedReport = {
+    report: Report;
+    classification: ReportClassification;
+    dampened: boolean;
+}
+
 export enum Dampen {
     False,
     True,
@@ -27,31 +33,30 @@ function tryDampenReport(report: Report): ReportClassification {
     let dampenIndex = 0;
     while (dampenIndex < report.levels.length) {
         let dampenedLevels = report.levels.toSpliced(dampenIndex, 1);
-        let trendInfo = determineTrend(dampenedLevels);
-        let classification = classifyTrend(trendInfo);
+        let classification = classifyTrend(determineTrend(dampenedLevels));
         if (classification === ReportClassification.Safe) {
             return classification;
         }
         dampenIndex++;
     }
-    console.log(report.levels);
     return ReportClassification.Unsafe;
 }
 
-export function classifyReportSafety(reports: Report[], dampen: Dampen = Dampen.False): ReportClassification[] {
+export function classifyReportSafety(reports: Report[], dampen: Dampen = Dampen.False): ClassifiedReport[] {
     let classified_reports = reports.map((report) => {
         let trendInfo = determineTrend(report.levels);
 
         const classification = classifyTrend(trendInfo);
         if (dampen === Dampen.False) {
-            return classification;
+            return { report: report, classification: classification, dampened: false };
         }
 
         if (trendInfo.trend !== Trend.None && trendInfo.maxDistance! <= MAX_DISTANCE_TO_CONSIDER_SAFE) {
-            return classification;
+            return { report: report, classification: classification, dampened: false };
         }
 
-        return tryDampenReport(report);
+        const dampenedClassification = tryDampenReport(report);
+        return { report: report, classification: dampenedClassification, dampened: true };
     });
     return classified_reports;
 }
